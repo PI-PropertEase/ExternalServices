924179b292d764a293697283c2f24043190db2f6
Migas77 <miguel.belchior@ua.pt>
2024-04-24 18:58:34 +0100

    PTSE-70 fix phone numbers and remove status of clickandgo reservations

--- apis/api_clickandgo/v1/reservations.py
+++ apis/api_clickandgo/v1/reservations.py
@@ -6,25 +6,25 @@ from threading import Lock
 from api_clickandgo.v1.properties import data
 
 reservation_data = {
-    1: ReservationInDB(id=1, property_id=10, status="OnGoing", client_name="John Smith", client_phone="5555555555",
+    1: ReservationInDB(id=1, property_id=10, client_email="smith_john@gmail.com", client_name="John Smith", client_phone="+351961555555",
                        arrival=datetime(2024, 3, 14, 12, 0), departure=datetime(2024, 3, 16, 12, 0), cost=200.0, amenities=["crib", "extra bed"]),
-    2: ReservationInDB(id=2, property_id=9, status="Pending", client_name="Jane Doe", client_phone="6666666666",
+    2: ReservationInDB(id=2, property_id=9, client_email="doe_jane@gmail.com", client_name="Jane Doe", client_phone="+351961666666",
                        arrival=datetime(2024, 4, 15, 12, 0), departure=datetime(2024, 4, 18, 12, 0), cost=220.0, amenities=[]),
-    3: ReservationInDB(id=3, property_id=8, status="OnGoing", client_name="Alice Brown", client_phone="7777777777",
+    3: ReservationInDB(id=3, property_id=8, client_email="brown_alice@gmail.com", client_name="Alice Brown", client_phone="+351961777777",
                        arrival=datetime(2024, 4, 16, 12, 0), departure=datetime(2024, 4, 19, 12, 0), cost=190.0, amenities=[]),
-    4: ReservationInDB(id=4, property_id=8, status="Pending", client_name="Bob Johnson", client_phone="8888888888",
+    4: ReservationInDB(id=4, property_id=8, client_email="johnson_bob@gmail.com", client_name="Bob Johnson", client_phone="+351961888888",
                        arrival=datetime(2024, 5, 17, 12, 0), departure=datetime(2024, 5, 20, 12, 0), cost=230.0, amenities=["crib", "extra bed"]),
-    5: ReservationInDB(id=5, property_id=7, status="OnGoing", client_name="Emily Wilson", client_phone="9999999999",
+    5: ReservationInDB(id=5, property_id=7, client_email="wilson_emily@gmail.com", client_name="Emily Wilson", client_phone="+351961999999",
                        arrival=datetime(2024, 6, 8, 12, 0), departure=datetime(2024, 6, 11, 12, 0), cost=210.0, amenities=[]),
-    6: ReservationInDB(id=6, property_id=6, status="Pending", client_name="Michael Martinez", client_phone="1111111111",
+    6: ReservationInDB(id=6, property_id=6, client_email="martinez_michael@gmail.com", client_name="Michael Martinez", client_phone="+351961111111",
                        arrival=datetime(2024, 4, 19, 12, 0), departure=datetime(2024, 4, 22, 12, 0), cost=240.0, amenities=["extra bed"]),
-    7: ReservationInDB(id=7, property_id=3, status="OnGoing", client_name="Olivia Davis", client_phone="2222222222",
+    7: ReservationInDB(id=7, property_id=3, client_email="davis_olivia@gmail.com", client_name="Olivia Davis", client_phone="+351961222222",
                        arrival=datetime(2024, 3, 20, 12, 0), departure=datetime(2024, 3, 23, 12, 0), cost=210.0, amenities=[]),
-    8: ReservationInDB(id=8, property_id=2, status="Pending", client_name="David Garcia", client_phone="3333333333",
+    8: ReservationInDB(id=8, property_id=2, client_email="garcia_david@gmail.com", client_name="David Garcia", client_phone="+351961333333",
                        arrival=datetime(2024, 3, 21, 12, 0), departure=datetime(2024, 3, 24, 12, 0), cost=260.0, amenities=[]),
-    9: ReservationInDB(id=9, property_id=1, status="OnGoing", client_name="Sophia Rodriguez", client_phone="4444444444",
+    9: ReservationInDB(id=9, property_id=1, client_email="rodriguez_sophia@gmail.com", client_name="Sophia Rodriguez", client_phone="+351961444444",
                        arrival=datetime(2024, 3, 22, 12, 0), departure=datetime(2024, 3, 25, 12, 0), cost=220.0, amenities=["crib"]),
-    10: ReservationInDB(id=10, property_id=10, status="Pending", client_name="James Hernandez", client_phone="1155555555",
+    10: ReservationInDB(id=10, property_id=10, client_email="hernandez_james@gmail.com", client_name="James Hernandez", client_phone="+351961115555",
                         arrival=datetime(2024, 3, 23, 12, 0), departure=datetime(2024, 3, 26, 12, 0), cost=280.0, amenities=[])
 }
 
